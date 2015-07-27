3626c1976c6edc526ee7dac79389531b38f10df1
unknown <User@louis.(none)>
2015-07-27 12:27:33 +0800

    Added computations

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,9 @@ public class HelloWorld{
 	public static void main(String[] args){
 
 		System.out.println("Hello World. :):):) "+ args[0]);
-
+		int x = 0;
+		int y = 3;
+		System.out.println(x+y);
 
 
 
