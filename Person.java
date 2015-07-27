8c90acc021dd74bb35e1ea384840152a7ad4aa48
unknown <User@louis.(none)>
2015-07-27 11:58:29 +0800

    Add address field

--- Person.java
+++ Person.java
@@ -2,6 +2,7 @@ public class Person{
   private String firstName;
   private String lastName;
   private String title;
+  private String address;
 
 public String getFullName(){
    return firstName + " " + lastName;	
