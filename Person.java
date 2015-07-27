30fce73cf722147ec2463e19cccb9387d590af83
Kerby Rangasa <kerby.rangasa@betica.com>
2015-07-27 12:39:02 +0800

    Added person nickname.

--- Person.java
+++ Person.java
@@ -2,8 +2,7 @@ public class Person{
   private String firstName;
   private String lastName;
   private String title;
-  private String address;
-
+   private String nickName;
 public String getFullName(){
    return firstName + " " + lastName;	
 
