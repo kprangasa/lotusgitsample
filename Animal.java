7b321d1122cbaf9196a4111c8fa7676ae319271e
Kerby Rangasa <kerby.rangasa@betica.com>
2015-07-27 13:08:03 +0800

    Revert "Merge branch 'master' into petsupport"
    This reverts commit 9e1901646b45a5261dc2198839eba25cac1979fe, reversing
    changes made to 30fce73cf722147ec2463e19cccb9387d590af83.

--- Animal.java
+++ Animal.java
@@ -2,16 +2,4 @@ public class Animal{
   private String name;
   private int age;
 
-
-public void eat(){
-	System.out.println("Eating");
-
-}
-
-public String getName(){
-	return this.name;
-
-
-}
-
 }
--- Person.java
+++ Person.java
@@ -2,32 +2,11 @@ public class Person{
   private String firstName;
   private String lastName;
   private String title;
-
-  private String address;
-
-   private List<Animal> animals = new ArrayList<Animal>();
-
-
    private String nickName;
-
 public String getFullName(){
    return firstName + " " + lastName;	
 
 
-}
-
-
-public void feedPets(){
-	for(Animal animal: animals){
-	animal.eat();
-	System.out.println("Feeding " + animal.getName());
-	}
-}
-
-public void introducePets(){
-	for(Animal pet: animals){
-	System.out.println(pet.getName());	
-}
 }
 
 
